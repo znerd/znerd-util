8c6b80b1b484137e8423e7d082ca7a133c89a768
Ernst de Haan <ernst@ernstdehaan.com>
2011-08-12 19:27:27 +0200

    Implemented 3 unit tests for ExceptionUtils.

--- src/test/java/org/znerd/util/ExceptionUtilsTest.java
+++ src/test/java/org/znerd/util/ExceptionUtilsTest.java
@@ -7,6 +7,24 @@ import static org.junit.Assert.*;
 public final class ExceptionUtilsTest {
 
     @Test
-    public void testGetRootCause() {
+    public void testGetRootCauseOfNull() {
+        assertEquals(null, ExceptionUtils.getRootCause(null));
+    }
+    
+    @Test
+    public void testGetRootCauseOfExceptionWithoutCause() {
+        Exception e = new Exception();
+        assertEquals(e, ExceptionUtils.getRootCause(e));
+    }
+    
+    @Test
+    public void testGetRootCauseOfExceptionWithCauses() {
+        Exception root = new Exception();
+        Exception e2 = new Exception(root);
+        Exception e3 = new Exception(e2);
+        Exception e4 = new Exception(e3);
+        Exception e5 = new Exception(e4);
+        
+        assertEquals(root, ExceptionUtils.getRootCause(e5));
     }
 }
