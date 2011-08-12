66a9fe6c88b96243ba15296e4c31a31e3760b077
Ernst de Haan <ernst@ernstdehaan.com>
2011-08-12 19:14:39 +0200

    Added first unit test.

---
+++ src/test/java/org/znerd/util/ExceptionUtilsTest.java
@@ -0,0 +1,12 @@
+// See the COPYRIGHT file for copyright and license information
+package org.znerd.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public final class ExceptionUtilsTest {
+
+    @Test
+    public void testGetRootCause() {
+    }
+}
