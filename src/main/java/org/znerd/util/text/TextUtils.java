061872a353c77a39fbd3862e2da9c90b95be8858
Ernst de Haan <ernst@ernstdehaan.com>
2011-08-12 18:36:59 +0200

    Fixed recursion error in TextUtils.quote(Object).

--- src/main/java/org/znerd/util/text/TextUtils.java
+++ src/main/java/org/znerd/util/text/TextUtils.java
@@ -4,7 +4,7 @@ package org.znerd.util.text;
 public class TextUtils {
 
     public static final String quote(Object o) {
-        return o == null ? "(null)" : quote(o.toString());
+        return o == null ? "(null)" : "\"" + o.toString() + '"';
     }
     
     public static final boolean isEmpty(String s) {
