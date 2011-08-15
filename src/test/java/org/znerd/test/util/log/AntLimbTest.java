97b20584e1fcc5730081a8402553eb5c24de48d4
Ernst de Haan <ernst@ernstdehaan.com>
2011-08-15 21:40:17 +0200

    Added empty AntLimbTest class.

---
+++ src/test/java/org/znerd/test/util/log/AntLimbTest.java
@@ -0,0 +1,11 @@
+// See the COPYRIGHT file for copyright and license information
+package org.znerd.test.util.log;
+
+import org.junit.Test;
+
+public class AntLimbTest {
+    @Test
+    public void testConvertToAntLevel() {
+
+    }
+}
