ef09eb78c47d3031bd7c1266d94448a583b3375a
Ernst de Haan <ernst@ernstdehaan.com>
2011-08-15 10:02:45 +0200

    Added CommonsExecCommandRunner and CommonsExecProcOutputBuffer.

---
+++ src/main/java/org/znerd/util/proc/CommonsExecCommandRunner.java
@@ -0,0 +1,65 @@
+// Copyright 2011, Ernst de Haan
+package org.znerd.util.proc;
+
+import java.io.IOException;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.Executor;
+import org.apache.commons.exec.ExecuteWatchdog;
+
+public class CommonsExecCommandRunner implements CommandRunner {
+
+    public CommonsExecCommandRunner(long timeOut) {
+        _timeOut = timeOut;
+    }
+
+    private long _timeOut;
+
+    @Override
+    public CommandRunResult runCommand(String command, String... arguments) {
+        long start = System.currentTimeMillis();
+        ExecuteWatchdog watchdog = createWatchdog();
+        CommonsExecProcOutputBuffer buffer = new CommonsExecProcOutputBuffer();
+        Executor executor = createExecutor(buffer, watchdog);
+        CommandLine commandLine = createCommandLine(command, arguments);
+        int exitCode;
+        IOException exception;
+        try {
+            exitCode = executor.execute(commandLine);
+            exception = null;
+        } catch (IOException cause) {
+            exitCode = -1;
+            exception = cause;
+        }
+
+        return createResult(start, buffer, exitCode, exception);
+    }
+
+    private ExecuteWatchdog createWatchdog() {
+        return (_timeOut > 0L) ? new ExecuteWatchdog(_timeOut) : null;
+    }
+
+    private CommandRunResult createResult(long start, CommonsExecProcOutputBuffer buffer, int exitCode, IOException exception) {
+        CommandRunResult result = new CommandRunResult();
+        result.setDuration(System.currentTimeMillis() - start);
+        result.setExitCode(exitCode);
+        result.setOutString(buffer.getStdoutAsString());
+        result.setErrString(buffer.getStderrAsString());
+        result.setException(exception);
+        return result;
+    }
+
+    private Executor createExecutor(CommonsExecProcOutputBuffer buffer, ExecuteWatchdog watchdog) {
+        DefaultExecutor executor = new DefaultExecutor();
+        executor.setStreamHandler(buffer);
+        executor.setWatchdog(watchdog);
+        return executor;
+    }
+
+    private CommandLine createCommandLine(String command, String... arguments) {
+        CommandLine commandLine = new CommandLine(command);
+        commandLine.addArguments(arguments);
+        return commandLine;
+    }
+}
